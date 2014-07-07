5abfb0c8148c02e8cb8ad1d96c7295cff1820154
Julio Bondia <julio.bondia13@gmail.com>
2014-07-07 13:48:44 +0200

    Added user favourite BPMs list simple management.

--- Blank_app/grails-app/domain/blank/User.groovy
+++ Blank_app/grails-app/domain/blank/User.groovy
@@ -4,18 +4,26 @@ class User {
 
     String name
     String login
+    Date dateCreated
 
     static hasMany = [bpms: Bpm]
     def favouriteBPMs = []
 
-    Date dateCreated
 
     static mapping = {
         autoTimestamp true
     }
 
     static constraints = {
-        name blank: false
-        login blank: false, unique: true
+        name(blank:false, minSize:2)
+        login(blank:false, unique:true, minSize:2)
+    }
+
+    def addBPMToFavourites(Bpm bpm) {
+        this.favouriteBPMs.add(bpm)
+    }
+
+    def getFavourites(){
+        return this.favouriteBPMs.sort{it.lastUpdated}
     }
 }
