1342c3bfe1dce01d351c58508aa84c911aba81d0
sergitoda <sergi.toda@estudiants.urv.cat>
2014-12-16 13:21:27 +0100

    Custom items are rendered showing which kind of element are

--- Ocelot_MT/grails-app/controllers/ocelot/CustomPaletteItemController.groovy
+++ Ocelot_MT/grails-app/controllers/ocelot/CustomPaletteItemController.groovy
@@ -26,12 +26,12 @@ class CustomPaletteItemController extends RestfulController{
         item.activated = jsonReq.activated
         item.props = jsonReq.props
 
-        //TODO dar al usuario la opcion de subir su svg y subir o seleccionar de una lista el icono
+        //TODO a user should upload it's own svg and icon
         item.icon = "hola"
-        item.svg = "hola"
+        item.svg = '<svg height="90" version="1.1" width="110" xmlns="http://www.w3.org/2000/svg" style="overflow: hidden; position: relative;"><desc style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">Created with Raphaël 2.1.0</desc><defs style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></defs><rect x="5" y="5" width="100" height="80" r="5" rx="5" ry="5" fill="#ffffff" stroke="#808080" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-opacity="1" id="svg_1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); stroke-linecap: round; stroke-linejoin: round; stroke-opacity: 1;"></rect><text x="55" y="45" text-anchor="middle" font="10px &quot;Arial&quot;" stroke="none" fill="#000000" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font-style: normal; font-variant: normal; font-weight: normal; font-size: 12px; line-height: normal; font-family: Arial, Helvetica, sans-serif;" font-size="12px" font-family="Arial, Helvetica, sans-serif"><tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">'+item.name+'</tspan></text></svg>'
 
         item.level = jsonReq.level
-        item.bpmnElem = "bpmn:UndefinedTask"
+        item.bpmnElem = "bpmn:Task"
 
         def category = CategoryItem.get(jsonReq.category.id)
         category.addToCustomPaletteItems(item)
