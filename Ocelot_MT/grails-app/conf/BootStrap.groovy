a5eadc4f3784d9e17ad00cbe661ddd830c44f3ff
Luis Alvarez <luis.alvarezabrio@gmail.com>
2015-03-10 19:37:22 +0100

    Created new branch.

--- Ocelot_MT/grails-app/conf/BootStrap.groovy
+++ Ocelot_MT/grails-app/conf/BootStrap.groovy
@@ -24,15 +24,46 @@ class BootStrap {
 				new CategoryItem(name: it).save()
 			}
 
-			File current = new File("grails-app/utils/")
-			def filesPath = current.getAbsolutePath()
-			current.delete()
+			def name, cat, icon, level, elem, props, fields, pItem
 
-			current = new File("scripts")
-			def scriptPath = current.getAbsolutePath()
-			current.delete()
+			def elements = new File("grails-app/utils/MainElements")
 
-			new ProcessBuilder(scriptPath + "/insertToDatabase", filesPath).start().waitFor()
+			elements.eachLine { line ->
+				if (!line.isEmpty()) {
+					if (!line.contains("svg")) {
+						fields = line.tokenize("?")
+						name = fields[0]
+						cat = fields[1].toLong()
+						icon = fields[2]
+						level = fields[3]
+						elem = "bpmn:"+fields[4]
+						props = fields[5]
+					} else {
+						pItem = new PaletteItem(name: name, category: CategoryItem.get(cat), icon: icon, level: level, bpmnElem: elem, props: props, svg: line, activated: true, description: "no desc")
+						pItem.save(flush: true, failOnError: true)
+						println(pItem)
+					}
+				}
+			}
+
+			def events = new File("grails-app/utils/Events")
+
+			events.eachLine { line ->
+				if (!line.isEmpty()) {
+					if (!line.contains("svg")) {
+						fields = line.tokenize("?")
+						name = fields[0]
+						cat = fields[1].toLong()
+						level = fields[2]
+						elem = "bpmn:"+fields[3]
+						props = fields[4]
+					} else {
+						pItem = new PaletteItem(name: name, category: CategoryItem.get(cat), icon: 'none(2).png', level: level, bpmnElem: elem, props: props, svg: line, activated: true, description: "no desc")
+						pItem.save(flush: true, failOnError: true)
+						println(pItem)
+					}
+				}
+			}
 
 			def palette = new Palette(name: "TestPalette")
 
